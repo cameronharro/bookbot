6cd78f5a4fdd3f0d4226e5db58fafd1898c0d3f7
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-04-04 16:52:22 -0500

    feat: add count_characters function

--- stats.py
+++ stats.py
@@ -2,3 +2,12 @@ def get_num_words(string):
   array = string.split()
   num_words = len(array)
   return num_words, f"{num_words} words found in the document"
+
+def count_characters(string):
+  chars = {}
+  for char in string:
+    if not char in chars:
+      chars[char] = 1
+    else:
+      chars[char] += 1
+  return chars
